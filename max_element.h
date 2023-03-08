50450fbdb1729933dee062a0decdbce02e6aff5f
BuranCodes <95971666+BuranCodes@users.noreply.github.com>
2023-03-08 18:33:43 +0100

    Update max_element.h

--- max_element.h
+++ max_element.h
@@ -1,6 +1,6 @@
 #ifndef MAX_ELEMENT_H_
 #define MAX_ELEMENT_H_
-int max_element(const int* sourceArr, const int numElements) // You have to manually set numElements or do sizeof before passing to the function
+int max_element(const int* sourceArr, const int numElements) // You have to manually set numElements or do sizeof before passing to the function, since C does not track array sizes
 {
     int returnMaxVal = *sourceArr;
     for (int Index = *(sourceArr+1); Index < numElements; Index++)
