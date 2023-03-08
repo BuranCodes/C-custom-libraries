241e89caa6a607a3de4f2a013feb16a02df92bd1
BuranCodes <95971666+BuranCodes@users.noreply.github.com>
2023-03-08 18:32:09 +0100

    Update max_element.h

--- max_element.h
+++ max_element.h
@@ -1,6 +1,6 @@
 #ifndef MAX_ELEMENT_H_
 #define MAX_ELEMENT_H_
-int max_element(const int* sourceArr, const int numElements) // You have to manually set numElemens or do sizeof before passing to the func
+int max_element(const int* sourceArr, const int numElements) // You have to manually set numElements or do sizeof before passing to the function
 {
     int returnMaxVal = *sourceArr;
     for (int Index = *(sourceArr+1); Index < numElements; Index++)
