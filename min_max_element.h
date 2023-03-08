3317b04b32a8c0d3342117f60d477b05eeec3b54
BuranCodes <95971666+BuranCodes@users.noreply.github.com>
2023-03-08 21:01:55 +0100

    merged files

--- min_max_element.h
+++ min_max_element.h
@@ -1,12 +1,6 @@
-<<<<<<< HEAD:max_element.h
-#ifndef MAX_ELEMENT_H_
-#define MAX_ELEMENT_H_
-int max_element(const int* sourceArr, const int numElements) // You have to manually set numElements or do sizeof before passing to the function, since C does not track array sizes
-=======
 #ifndef MIN_MAX_ELEMENT_H_
 #define MIN_MAX_ELEMENT_H_
 int max_element(const int* sourceArr, const int numElements) // You have to manually set numElemens or do sizeof before passing to the func
->>>>>>> a9d2478 (merged files):min_max_element.h
 {
     int returnMaxVal = *sourceArr;
     for (int Index = *(sourceArr+1); Index < numElements; Index++)
@@ -15,9 +9,6 @@ int max_element(const int* sourceArr, const int numElements) // You have to manu
     }
     return returnMaxVal;
 };
-<<<<<<< HEAD:max_element.h
-#endif
-=======
 
 int min_element(const int* sourceArr, const int numElements)
 {
@@ -29,4 +20,3 @@ int min_element(const int* sourceArr, const int numElements)
     return returnMinVal;
 };
 #endif
->>>>>>> a9d2478 (merged files):min_max_element.h
