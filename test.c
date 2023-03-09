a4705419e5bbf38614a24f768319cacf5cb361e1
BuranCodes <95971666+BuranCodes@users.noreply.github.com>
2023-03-10 00:17:31 +0100

    Update test.c

--- test.c
+++ test.c
@@ -7,13 +7,13 @@ int main(void)
         int something = *Array;
         printf("%d\n", something);
 
-        int Variable1 = min_element(Array, 16);
-        int Variable2 = max_element(Array, 16);
-        int Variable3 = 5;
-        printf("Smallest variable in the array: %d\n", Variable1);
-        printf("Largest variable in the array: %d\n", Variable2);
-        printf("Position for value 5 found at: %d\n", find_one_element(Array, 16, Variable3));
-        printf("Count for value 5: %d\n", count_elements(Array, 16, Variable3));
+        int var1 = min_element(Array, 16);
+        int var2 = max_element(Array, 16);
+        int var3 = 5;
+        printf("Smallest variable in the array: %d\n", var1);
+        printf("Largest variable in the array: %d\n", var2);
+        printf("Position for value 5 found at: %d\n", target(Array, 16, var3));
+        printf("Count for value 5: %d\n", count_elements(Array, 16, var3));
 
         
 
