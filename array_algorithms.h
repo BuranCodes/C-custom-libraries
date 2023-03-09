00663b9c562d79e9ae07b00fb724f2a5e4758cd3
BuranCodes <95971666+BuranCodes@users.noreply.github.com>
2023-03-09 17:13:07 +0100

    Updates

--- array_algorithms.h
+++ array_algorithms.h
@@ -5,21 +5,35 @@
 
 #define ARR_BEGIN 0
 #define ARR_BEGIN_PAST_ZERO 1
+#define POS_NOT_FOUND -1
 
-int max_element(const int *sourceArr, const int numElements) // You have to manually set numElemens or do sizeof before passing to the function
+int max_element(const int *__source_arr, const int __num_elements) // You have to manually set numElemens or do sizeof before passing to the function
 {
-        int returnValue = *sourceArr;
-        for (int Index = ARR_BEGIN_PAST_ZERO; Index < numElements; Index++)
-                if (returnValue < sourceArr[Index]) returnValue = sourceArr[Index];
-        return returnValue;
-};
+        int __return_value = *__source_arr;
+        for (int Index = ARR_BEGIN_PAST_ZERO; Index < __num_elements; Index++)
+                if (__return_value < __source_arr[Index]) __return_value = __source_arr[Index];
+        return __return_value;
+}
 
-int min_element(const int *sourceArr, const int numElements)
+int min_element(const int *__source_arr, const int __num_elements)
 {
-        int returnValue = *sourceArr;
-        for (int Index = ARR_BEGIN_PAST_ZERO; Index < numElements; Index++)
-                if (returnValue > sourceArr[Index]) returnValue = sourceArr[Index];
-        return returnValue;
-};
+        int __return_value = *__source_arr;
+        for (int Index = ARR_BEGIN_PAST_ZERO; Index < __num_elements; Index++)
+                if (__return_value > __source_arr[Index]) __return_value = __source_arr[Index];
+        return __return_value;
+}
+
+int find_one_element(const int* ____source_arr, const int ____num_elements, int __find_value) 
+{
+        int __return_value;
+        for (int Index = ARR_BEGIN; Index < ____num_elements; Index++) {
+                if (__find_value == ____source_arr[Index]) {
+                        __return_value = Index;
+                        break;
+        }
+        if (!__return_value)
+                return POS_NOT_FOUND;
+        else return __return_value;
+}
 
 #endif
