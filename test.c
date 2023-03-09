7f1aa263a960ee6b528c95458ebafc59d13c8211
BuranCodes <95971666+BuranCodes@users.noreply.github.com>
2023-03-09 16:45:57 +0100

    updates

--- test.c
+++ test.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
-#include "min_max_element.h"
+#include "array_algorithms.h"
 
-int main(void) {
-    int Array[5] = {5,3,4,1,2};
+int main(void)
+{
+        int Array[16] = {2,17,5,1,2,5,7,1,2,7,9,4,6,8,4,0};
+        int something = *Array;
+        printf("%d\n", something);
 
-    int Variable1 = min_element(Array, 5);
-    int Variable2 = max_element(Array, 5);
-    printf("Smallest variable in the array: %d\n", Variable1);
-    printf("Largest variable in the array: %d\n", Variable2);
+        int Variable1 = min_element(Array, 16);
+        int Variable2 = max_element(Array, 16);
+        printf("Smallest variable in the array: %d\n", Variable1);
+        printf("Largest variable in the array: %d\n", Variable2);
 
-    // Success!
+        // Success!
 }
